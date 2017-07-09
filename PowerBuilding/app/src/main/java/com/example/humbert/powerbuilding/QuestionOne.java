dc95ca9ebcb1a74750d26c33dbc443f3c8cde9a3
Zane <humbert_qm@hotmail.com>
2017-07-09 03:43:49 +0200

    Botones Animados
    Next Steps: Acabar Formulario
    Lunes: Mirar de guardar en database "Person Info"

--- PowerBuilding/app/src/main/java/com/example/humbert/powerbuilding/QuestionOne.java
+++ PowerBuilding/app/src/main/java/com/example/humbert/powerbuilding/QuestionOne.java
@@ -4,25 +4,30 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class QuestionOne extends AppCompatActivity {
-    ImageButton male,female;
-    TextView question;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_one);
-        question = (TextView) findViewById(R.id.question);
-    }
 
-    public void onClick(View v){
+        TextView question = (TextView) findViewById(R.id.question);
 
-        male = (ImageButton) findViewById(R.id.btn_male);
-        female= (ImageButton) findViewById(R.id.btn_female);
-        Intent intent= new Intent(QuestionOne.this, QuestionTwo.class);
-        startActivity(intent);
     }
-}
+            public void onClick(View v) {
+                Button male = (Button) findViewById(R.id.btn_male);
+                Button female = (Button) findViewById(R.id.btn_female);
+                Intent intent = new Intent(QuestionOne.this, QuestionTwo.class);
+                startActivity(intent);
+
+
+
+            }
+
+
+
+    }
--- PowerBuilding/app/src/main/java/com/example/humbert/powerbuilding/Welcome.java
+++ PowerBuilding/app/src/main/java/com/example/humbert/powerbuilding/Welcome.java
@@ -6,26 +6,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Welcome extends AppCompatActivity {
-    TextView welcome;
-    ImageView icono;
-    ImageButton btn;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        welcome = (TextView) findViewById(R.id.welcome);
-        icono= (ImageView) findViewById(R.id.icon);
+        TextView welcome = (TextView) findViewById(R.id.welcome);
+        ImageView icono = (ImageView) findViewById(R.id.icon);
 
 
 
     }
     public void onClick(View v){
-        btn= (ImageButton) findViewById(R.id.btnStart);
+        Button start = (Button) findViewById(R.id.btn_start);
         Intent intent = new Intent(Welcome.this, QuestionOne.class);
         startActivity(intent);
     }
