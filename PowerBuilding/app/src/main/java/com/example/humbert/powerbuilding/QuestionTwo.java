51cbc8bc930975b774af51c8b71b7582b0df878e
Zane <humbert_qm@hotmail.com>
2017-07-10 03:28:16 +0200

    Mejora botones
    Corregir errores + continuar con formulario

--- PowerBuilding/app/src/main/java/com/example/humbert/powerbuilding/QuestionTwo.java
+++ PowerBuilding/app/src/main/java/com/example/humbert/powerbuilding/QuestionTwo.java
@@ -2,17 +2,41 @@ package com.example.humbert.powerbuilding;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ToggleButton;
 
 public class QuestionTwo extends AppCompatActivity {
-    ToggleButton kg,lb;
+    ToggleButton weight;
+    EditText weight_number;
+    Button accept;
+    Boolean ToggleButtonState;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_two);
-       /* kg= (ToggleButton) findViewById(R.id.tbtn_kg);
-         lb=(ToggleButton) findViewById(R.id.tbtn_lb);
-        Boolean ToggleButtonState= kg.isChecked();
-        Boolean ToggleButtonState = lb.isChecked(); */
+
+        EditText weight_number = (EditText) findViewById(R.id.text_weight);
+        Button accept = (Button) findViewById(R.id.btn_accept);
     }
-}
+
+    //Arreglar que cuando este clicado Kg aparezca hint de Kg y viceversa con Lbs
+        public void onClick(View view){
+        weight = (ToggleButton) findViewById(R.id.tbtn_weight);
+        if(weight.isChecked()){
+            weight_number.setHint("Kg");
+        }
+        else{
+            weight_number.setHint("Lbs");
+        }
+        }
+    }
+
+
+
+
+
+
