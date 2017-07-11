7d070f1afa5bc8a4c4d3bbf5a324606c6e413b52
Zane <humbert_qm@hotmail.com>
2017-07-11 16:05:18 +0200

    Declaración tbtn

--- PowerBuilding/app/src/main/java/com/example/humbert/powerbuilding/IntroduceWorkout.java
+++ PowerBuilding/app/src/main/java/com/example/humbert/powerbuilding/IntroduceWorkout.java
@@ -2,14 +2,35 @@ package com.example.humbert.powerbuilding;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ToggleButton;
 
-public class IntroduceWorkout extends AppCompatActivity {
+public class IntroduceWorkout extends AppCompatActivity implements View.OnClickListener {
+    ToggleButton monday,tuesday,wednesday,thursday,friday,saturday,sunday;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_introduce_workout);
+        monday=(ToggleButton) findViewById(R.id.tbtn_monday);
+        tuesday=(ToggleButton) findViewById(R.id.tbtn_tuesday);
+        wednesday=(ToggleButton) findViewById(R.id.tbtn_wednesday);
+        thursday=(ToggleButton) findViewById(R.id.tbtn_thursday);
+        friday=(ToggleButton) findViewById(R.id.tbtn_friday);
+        saturday=(ToggleButton) findViewById(R.id.tbtn_saturday);
+        sunday= (ToggleButton) findViewById(R.id.tbtn_sunday);
+        monday.setOnClickListener(this);
+        tuesday.setOnClickListener(this);
+        wednesday.setOnClickListener(this);
+        thursday.setOnClickListener(this);
+        friday.setOnClickListener(this);
+        saturday.setOnClickListener(this);
+        sunday.setOnClickListener(this);
+        
     }
+    public void onClick(View v){
 
+
+    }
 
 }
